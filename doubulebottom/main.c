217d5e6a7d1fa10d4ca8e84e98f4e63dc1d90bd0
goldchari0t <51641034+goldchari0t@users.noreply.github.com>
2020-10-21 00:35:09 +0800

    DD

--- doubulebottom/main.c
+++ doubulebottom/main.c
@@ -16,11 +16,11 @@ int stfb=0;//设定脉冲  b
 
 
 
-void wakeupa(int h,int j)  // 激活死去的a 并且设定速度 和脉冲数量   
+void wakeupa(int h,int j)  // 激活死去的a 并且设定速度 和脉冲数量
 //h 给 timefa j 给 stfa
 {TIMSK=(1<<OCIE1A);//双比较匹配使能 再次启动
- OCR1A=h;     //初始化0.005s  一个脉冲
- stfa=j;
+	OCR1A=h;     //初始化0.005s  一个脉冲
+	stfa=j;
 	
 }
 void wakeupb(int h,int j)  // 激活死去的b 并且设定速度 和脉冲数量
@@ -31,37 +31,43 @@ void wakeupb(int h,int j)  // 激活死去的b 并且设定速度 和脉冲数量
 	
 }
 void TC1_init()
-{ asm("sei");// 全局中断使能
- TIMSK=(1<<OCIE1A)|(1<<OCIE1B);//双比较匹配使能
- TCCR1A=(1<<COM1A0)|(1<<COM1B0);//匹配时电平取反
- TCCR1B=(1<<WGM12)|(1<<CS11);//1/8分频器
- OCR1A=50000;//初始化0.005s一个脉冲
- OCR1B=50000;//初始化0.005s一个脉冲
+{
+	asm("sei");// 全局中断使能
+	TIMSK=(1<<OCIE1A)|(1<<OCIE1B);//双比较匹配使能
+	TCCR1A=(1<<COM1A0)|(1<<COM1B0);//匹配时电平取反
+	TCCR1B=(1<<WGM12)|(1<<CS11);//1/8分频器
+	OCR1A=50000;//初始化0.005s一个脉冲
+	OCR1B=50000;//初始化0.005s一个脉冲
 }
 ISR(TIMER1_COMPA_vect)
-{   cofa++;
-      if(cofa>stfa)
-    {TCCR1A=0x00;//停止翻转
-     cofa=0;//count 归零
-    }
+{
+	//cofa++;
+	//if(cofa>stfa)
+	//{TCCR1A=0x00;//停止翻转
+	//cofa=0;//count 归零
+	//}
+	DDRB |= 0XFF;
+	PORTB |=0XFF;
 }
 
 ISR(TIMER1_COMPB_vect)
-{   cofb++;
-	if(cofb>stfb)
-	{TCCR1B=0x00;//停止翻转
-		cofb=0;//count 归零
-	}
-	
+{   
+	//cofb++;
+	//if(cofb>stfb)
+	//{TCCR1B=0x00;//停止翻转
+		//cofb=0;//count 归零
+	//}
+	DDRB |= 0XFF;
+	PORTB |=0XFF;
 }
 int main(void)
 {
 	TC1_init();
-    wakeupa(50000,800);
-    wakeupb(50000,800);
-	while (1) 
-    { 
+	//wakeupa(50000,800);
+	//wakeupb(50000,800);
+	while (1)
+	{
 		
-    }
+	}
 }
 
