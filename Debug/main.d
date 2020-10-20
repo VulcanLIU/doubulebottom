90f7d8d11368b865aa4acb34b13f10861d603343
goldchari0t <51641034+goldchari0t@users.noreply.github.com>
2020-10-21 00:25:46 +0800

    Initial commit

---
+++ Debug/main.d
@@ -0,0 +1,46 @@
+main.d main.o: .././main.c \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\software\astudio\7.0\Packs\atmel\ATmega_DFP\1.3.300\include/avr/iom32.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\software\astudio\7.0\Packs\atmel\ATmega_DFP\1.3.300\include/avr/iom32.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\software\astudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ main.c
@@ -0,0 +1,67 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define  F_CPU 16000000UL  //定义系统时钟
+#include <util/delay.h>
+void TC0_init(void);
+void TC1_init(void);
+void  wakeupb(int h,int j);
+void  wakeupa(int h,int j);
+int timefa=0;// 脉冲速率for a
+int timefb=0;// 脉冲速率for b
+int cofa=0;//对脉冲数计数的 a
+int cofb=0;//对脉冲数计数的 b
+int stfa=0;//设定脉冲  a
+int stfb=0;//设定脉冲  b
+
+
+
+void wakeupa(int h,int j)  // 激活死去的a 并且设定速度 和脉冲数量   
+//h 给 timefa j 给 stfa
+{TIMSK=(1<<OCIE1A);//双比较匹配使能 再次启动
+ OCR1A=h;     //初始化0.005s  一个脉冲
+ stfa=j;
+	
+}
+void wakeupb(int h,int j)  // 激活死去的b 并且设定速度 和脉冲数量
+//h 给 timefa j 给 stfa
+{TIMSK=(1<<OCIE1B);//双比较匹配使能 再次启动
+	OCR1B=h;     //初始化0.005s  一个脉冲
+	stfb=j;
+	
+}
+void TC1_init()
+{ asm("sei");// 全局中断使能
+ TIMSK=(1<<OCIE1A)|(1<<OCIE1B);//双比较匹配使能
+ TCCR1A=(1<<COM1A0)|(1<<COM1B0);//匹配时电平取反
+ TCCR1B=(1<<WGM12)|(1<<CS11);//1/8分频器
+ OCR1A=50000;//初始化0.005s一个脉冲
+ OCR1B=50000;//初始化0.005s一个脉冲
+}
+ISR(TIMER1_COMPA_vect)
+{   cofa++;
+      if(cofa>stfa)
+    {TCCR1A=0x00;//停止翻转
+     cofa=0;//count 归零
+    }
+}
+
+ISR(TIMER1_COMPB_vect)
+{   cofb++;
+	if(cofb>stfb)
+	{TCCR1B=0x00;//停止翻转
+		cofb=0;//count 归零
+	}
+	
+}
+int main(void)
+{
+	TC1_init();
+    wakeupa(50000,800);
+    wakeupb(50000,800);
+	while (1) 
+    { 
+		
+    }
+}
+
